f61871e8a38ca792ab678738c4e5347fc0811e98
marcalph <alphonsus.marc@gmail.com>
2026-02-04 09:42:44 +0100

    add rsync script + single thread vector add

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Deploy kernelforge to tower
 
-REMOTE="tower"
-REMOTE_PATH="~/kernelforge"
+REMOTE="tower.local"
+REMOTE_PATH="~/projects/kernelforge"
 
 # Sync project (exclude build artifacts and git)
 rsync -avz --progress \
