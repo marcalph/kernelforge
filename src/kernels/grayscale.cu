90e35e0768e74fa7a9d6374598dd528a31af83c6
marcalph <alphonsus.marc@gmail.com>
2026-03-04 10:32:18 +0100

    wip

--- src/kernels/grayscale.cu
+++ src/kernels/grayscale.cu
@@ -1,48 +1,54 @@
+#include <c10/cuda/CUDAException.h>
+#include <c10/cuda/CUDAStream.h>
+
+
 __global__
-void rgb_to_grayscale_kernel(
-    unsigned char* __restrict__ output,
-    const unsigned char* __restrict__ input,
-    int width, int height
-) {
+void rgb_to_grayscale_kernel(unsigned char* output, unsigned char* input, int width, int height) {
+    const int channels = 3;
+
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     if (col < width && row < height) {
-        int i = row * width + col;
-        int inputOffset = i * 3;
-        // ITU-R BT.709 luminance
-        output[i] = (unsigned char)(0.2126f * input[inputOffset]
-                                  + 0.7152f * input[inputOffset + 1]
-                                  + 0.0722f * input[inputOffset + 2]);
+        int outputOffset = row * width + col;
+        int inputOffset = (row * width + col) * channels;
+
+        unsigned char r = input[inputOffset + 0];   // red
+        unsigned char g = input[inputOffset + 1];   // green
+        unsigned char b = input[inputOffset + 2];   // blue
+
+        output[outputOffset] = (unsigned char)(0.21f * r + 0.71f * g + 0.07f * b);
     }
 }
 
 
+// helper function for ceiling unsigned integer division
 inline unsigned int cdiv(unsigned int a, unsigned int b) {
-    return (a + b - 1) / b;
+  return (a + b - 1) / b;
 }
 
 
 torch::Tensor rgb_to_grayscale(torch::Tensor image) {
-    TORCH_CHECK(image.device().type() == torch::kCUDA);
-    TORCH_CHECK(image.dtype() == torch::kByte);
+    assert(image.device().type() == torch::kCUDA);
+    assert(image.dtype() == torch::kByte);
 
     const auto height = image.size(0);
-    const auto width  = image.size(1);
+    const auto width = image.size(1);
 
-    auto result = torch::empty({height, width, 1},
-        torch::TensorOptions().dtype(torch::kByte).device(image.device()));
+    auto result = torch::empty({height, width, 1}, torch::TensorOptions().dtype(torch::kByte).device(image.device()));
 
-    dim3 threads_per_block(16, 16);
+    dim3 threads_per_block(16, 16);     // using 256 threads per block
     dim3 number_of_blocks(cdiv(width, threads_per_block.x),
                           cdiv(height, threads_per_block.y));
 
-    rgb_to_grayscale_kernel<<<number_of_blocks, threads_per_block, 0,
-                              c10::cuda::getCurrentCUDAStream()>>>(
+    rgb_to_grayscale_kernel<<<number_of_blocks, threads_per_block, 0, torch::cuda::getCurrentCUDAStream()>>>(
         result.data_ptr<unsigned char>(),
         image.data_ptr<unsigned char>(),
-        width, height
+        width,
+        height
     );
 
+    // check CUDA error status (calls cudaGetLastError())
     C10_CUDA_KERNEL_LAUNCH_CHECK();
+
     return result;
 }
