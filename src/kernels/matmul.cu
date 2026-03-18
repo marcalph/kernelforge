20f6a85189a82025fe2a2600d0ff33a4aa42e09d
marcalph <alphonsus.marc@gmail.com>
2026-03-18 16:39:58 +0100

    chap3 ex

--- src/kernels/matmul.cu
+++ src/kernels/matmul.cu
@@ -22,7 +22,7 @@ void matmul_k(float *m, float *n, float * out, int h, int w, int k) {
 }
 
 
-__global__ matmul_1thread1row_k(float *m, float *n, float * out, int h, int w,  int k) {
+__global__  void matmul_1thread1row_k(float *m, float *n, float * out, int h, int w,  int k) {
     int row = blockIdx.x * blockDim.x + threadIdx.x;
     if (row>=h) return; // out of bound
     for (int col=0; col<k; col++){
